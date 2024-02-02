5a96cbb0cb0d9035390f58a648962ff9765ed99e
chriskim2273 <christopher2273@gmail.com>
2024-02-01 20:00:59 -0500

    Changed to fallback to Streamlit secrets

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Set environment variables
-AUTHORIZE_URL = os.environ.get('AUTHORIZE_URL')
-TOKEN_URL = os.environ.get('TOKEN_URL')
-REFRESH_TOKEN_URL = os.environ.get('REFRESH_TOKEN_URL')
-REVOKE_TOKEN_URL = os.environ.get('REVOKE_TOKEN_URL')
-CLIENT_ID = os.environ.get('CLIENT_ID')
-CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
-REDIRECT_URI = os.environ.get('REDIRECT_URI')
-SCOPE = os.environ.get('SCOPE')
+AUTHORIZE_URL = os.environ.get('AUTHORIZE_URL',st.secrets["AUTHORIZE_URL"])
+TOKEN_URL = os.environ.get('TOKEN_URL',st.secrets["TOKEN_URL"])
+REFRESH_TOKEN_URL = os.environ.get('REFRESH_TOKEN_URL',st.secrets["REFRESH_TOKEN_URL"])
+REVOKE_TOKEN_URL = os.environ.get('REVOKE_TOKEN_URL',st.secrets["REVOKE_TOKEN_URL"])
+CLIENT_ID = os.environ.get('CLIENT_ID',st.secrets["CLIENT_ID"])
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET',st.secrets["CLIENT_SECRET"])
+REDIRECT_URI = os.environ.get('REDIRECT_URI',st.secrets["REDIRECT_URI"])
+SCOPE = os.environ.get('SCOPE',st.secrets["SCOPE"])
 
 # Create OAuth2Component instance
 oauth2 = OAuth2Component(CLIENT_ID, CLIENT_SECRET, AUTHORIZE_URL, TOKEN_URL, REFRESH_TOKEN_URL, REVOKE_TOKEN_URL)
