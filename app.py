c8e1bb710b23facc79289188be65852099c61e9a
chriskim2273 <christopher2273@gmail.com>
2024-02-04 03:47:06 -0500

    Testing progress bar for uploading to google calendar

--- app.py
+++ app.py
@@ -249,11 +249,15 @@ if uploaded_file:
         upload_shifts_to_gcal = st.button("Upload to Google Calendar")
         if upload_shifts_to_gcal:
             progress_text = "Uploading Shifts to Google Calendar..."
-            my_bar = st.progress(0, text=progress_text)
+            progress_bar = st.progress(0, text=progress_text)
 
             for percent_complete in range(100):
                 time.sleep(0.01)
-                my_bar.progress(percent_complete + 1, text=progress_text)
+                progress_bar.progress(percent_complete + 1, text=progress_text)
+            time.sleep(1)
+            progress_bar.empty()
+            st.balloons()
+            st.success('This is a success message!', icon="✅")
 
 
 def create_events():
